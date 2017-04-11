8870865698d43ba40350d3f49a23f920ad435bd4
smeth194243 <smor7432@740s-pl-106.local>
2017-04-11 11:36:59 -0600

    k fixed it

---
+++ src/inherit/model/SethMorris.java
@@ -0,0 +1,28 @@
+package inherit.model;
+
+import inherit.model.LoudNoises;
+
+public class SethMorris implements LoudNoises
+{
+	public double clapVolume(Double decibleLevel)
+	{
+		decibleLevel = 999.999;
+				return decibleLevel;
+	}
+	public double shotgunBlast(Double power)
+	{
+		power = 420.69;
+		return power;
+	}
+	public String carHorn(String hornSound)
+	{
+		hornSound = "Mexican jingle tune";
+		return hornSound;
+	}
+	public boolean duncanClap(boolean didEardrumsBreak)
+	{
+		didEardrumsBreak = true;
+		return didEardrumsBreak;
+	}
+}
+// please work
